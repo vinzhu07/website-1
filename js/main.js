9e9c4a0382af3ab981071b75ea0b7bd641caac3d
Alvin Bao <alvinbao@alvins-mbp.lan>
2020-07-09 17:00:50 -0400

    added mute/unmute video functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+var button = document.getElementById('speaker2');
+var video = document.getElementById('video-background')
+    button.onclick = function (){
 
+    if (video.muted === false) {
+           video.muted = true;
+           button.className = "speaker mute"
+
+    }
+
+    else {
+        video.muted = false;
+        button.className = "speaker"
+    }
+
+    };
